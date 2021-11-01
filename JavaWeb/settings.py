33e35f50cde2ab34c36b3671f5a0237875b5fa55
devansh9999 <devchopra999@gmail.com>
2021-11-01 16:25:30 +0530

    django_heroku

--- JavaWeb/settings.py
+++ JavaWeb/settings.py
@@ -29,8 +29,9 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG =False
 
 ALLOWED_HOSTS = [
-    host.strip()
-    for host in config("ALLOWED_HOSTS",default='').split(',')
+    '*'
+    # host.strip()
+    # for host in config("ALLOWED_HOSTS",default='').split(',')
 ]
 
 
